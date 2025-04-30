66a156a3f750c456f1a77f3c4ec1dd8b0c18272b
sonalsaini29 <sonalsaini29@gmail.com>
2025-04-30 17:16:23 +0200

    Changes

---
+++ tests/Utils/webelements.py
@@ -0,0 +1,9 @@
+# webelements.py
+
+class AmazonHomePage:
+    EMAIL_INPUT = "input[name='email']"
+    CONTINUE_BUTTON = "input#continue"
+    PASSWORD_INPUT = "input[name='password']"
+    SIGN_IN_BUTTON = "input#signInSubmit"
+    CATEGORY_MENU = "a#nav-hamburger-menu"
+    SIGN_IN_LINK = ("role=link[name='Hello, sign in Account & Lists']")
